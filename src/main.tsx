8993836e61502d8f1af2943bb5bb8c7157fd3f90
Ashkan Mahdizadeh <ac.programmer.ashkan@gmail.com>
2023-08-16 19:22:11 +0430

    setup -3
    -add redux
    -add redux/toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux';
 
+import store from '@/store';
 import Layout from '@/Layout';
 import '@/assets/styles/main.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>  
+    <Provider store={store}>
       <Layout />
+    </Provider>
   </React.StrictMode>,
 )
